320764547f6102f0deddbf6c5e32966987bd53a2
wallmenis <sandboxgamergr@hotmail.com>
2022-12-02 13:48:37 +0200

    added selection for manual/guided disk formating (only manual working kinda)

--- archinstall.sh
+++ archinstall.sh
@@ -3,15 +3,22 @@
 ping -c 3 archlinux.org > /dev/null
 
 if [[ $? -eq 0 ]]; then
-echo "internet connection established"
+    echo "Internet connection established!"
 else
-echo "no internet connection found"
+    echo "No internet connection found! Please connect to a network with internet and run this script again"
+    exit
 fi
-
-echo "Please format the drives and press Ctrl+D when you're done"
-
-bash
-
-echo "Is the following drive formatting ok for you?"
-lsblk
-
+INP="n"
+while [ $INP != "y" ]; do
+    echo "Will now enter disk formating. Would you like a manual[1] or guided[2] install? [1/2] [default=2]"
+    read $INP
+    if [[ $INP -eq 1 ]]; then
+        echo "Entering manual disk formating. Press Ctrl+D when done."
+        bash
+    else
+        echo "Entering guided disk formating. Please select drive:"
+    fi
+    echo "Is the following drive formatting ok for you? [y/n]"
+    lsblk
+    read INP
+done
